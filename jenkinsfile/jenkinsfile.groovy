c93efac7c2a1c0e777b0621de6009a59a252e023
bommasani vamsi krishna <bommasani@bommasanis-MacBook-Pro.local>
2019-05-12 18:28:26 -0400

    Final Jenkins File Changes

--- jenkinsfile/jenkinsfile.groovy
+++ jenkinsfile/jenkinsfile.groovy
@@ -2,65 +2,74 @@ pipeline {
 agent any
 tools {
     gradle "Gradle-5.3.1"
+    maven "3.6.1"
+    ant "1.10.5"
   }
 parameters {
   choice(choices: '\n-v\n-vv\n-vvv\n-vvvv', description: 'Choose a verbosity level', name: 'VERBOSITY')
-  string(name: 'versionnumber',defaultValue: "",description: 'Choose a version number to upload latest version')
 }
 options {
     buildDiscarder(logRotator(numToKeepStr:'10' , artifactNumToKeepStr: '10'))
     timestamps()
     }
 environment {
-      inventoryName    = 'sandbox'
+      inventoryName    = 'Bommasani'
 }
   stages {
     stage('Checkout') {
       steps {
         echo 'Checking out project from Bitbucket....'
-        dir("gradle") {
-          git branch: 'develop', url: 'ssh://git@bitbucket.us.comerica.net:7999/dcs/debitcardservices.git'
+        dir("vamsi") {
+          git branch: 'master', url: 'git@github.com:vamsi8977/jenkinsfile.git'
         }
       }
     }
-stage('gradle clean build') {
+stage('shell') {
       steps {
         ansiColor('xterm') {
           echo 'Cleaning workspace....'
            sh """
-          cd ${WORKSPACE}/dcs/;
-          gradle clean build
-          chmod 755 ${WORKSPACE}/dcs/build/libs/*
-          cd ${WORKSPACE}/dcs/build/libs/
-          ls
+           java -version
+           mvn --version
+           gradle -version
+           ant -version
+           python --version
+           ansible --version
+           git --version
+           terraform -v
           """
         }
       }
     }
-    stage('AWS Session') {
-      steps {
-        awsSession("cma-dev")
-      }
-    }
-    stage('AWS CLI Session') {
-      steps {
-        script {
-          def dockerCommand = """\
-            source /workspace/exportAwsSession.sh;
-            aws s3 cp /workspace/dcs/build/libs/debitcardservices.jar s3://cma-dev/dcs/ --no-verify-ssl
-            """.stripIndent()
-          // Call Docker
-          callDocker("", "1.2", dockerCommand)
-        }
-      }
-    }
-    stage('JFrog Artifactory') {
+stage('maven') {
       steps {
         ansiColor('xterm') {
           echo 'Cleaning workspace....'
            sh """
-           curl -v -u deployment:deployment123 --upload-file ${WORKSPACE}/dcs/build/libs/debitcardservices.jar https://artifactory.us.comerica.net/artifactory/Applications/com/comerica/DCS/${params.versionnumber}/debitcardservices-${params.versionnumber}.jar
-           curl -v -u deployment:deployment123 --upload-file ${WORKSPACE}/dcs/build/libs/debitcardservices.jar https://artifactory.us.comerica.net/artifactory/Applications/com/comerica/DCS/${params.versionnumber}/debitcardservices.jar
+          cd ${WORKSPACE}/vamsi/maven;
+          mvn clean install
+          """
+        }
+      }
+    }
+stage('gradle') {
+      steps {
+        ansiColor('xterm') {
+          echo 'Cleaning workspace....'
+           sh """
+          cd ${WORKSPACE}/vamsi/gradle;
+          gradle clean build
+          """
+        }
+      }
+    }
+stage('ant') {
+      steps {
+        ansiColor('xterm') {
+          echo 'Cleaning workspace....'
+           sh """
+          cd ${WORKSPACE}/vamsi/ant;
+          ant -buildfile build.xml
           """
         }
       }
@@ -68,11 +77,13 @@ stage('gradle clean build') {
   }//end stages
   post {
       success {
-          archiveArtifacts artifacts: "dcs/build/libs/*.jar"
+          archiveArtifacts artifacts: "vamsi/ant/build/jar/*.jar"
+          archiveArtifacts artifacts: "vamsi/gradle/build/libs/*.jar"
+          archiveArtifacts artifacts: "vamsi/maven/target/*.war"
       }
       failure {
           echo "The build failed."
-          mail to: 'shaddad@comerica.com vkbommasani@comerica.com ITPDTIM_CardServicesSupport@comerica.com ',
+          mail to: 'bommasani.vamsikrishna@gmail.com',
                   subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
                   body: "Something is wrong with ${env.BUILD_URL}"
       }
